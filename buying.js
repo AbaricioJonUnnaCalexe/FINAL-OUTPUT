5d368611b8e9fffad4b3927e8a66cfe6f17e0041
AbaricioJonUnnaCalexe <unnavlog@gmail.com>
2026-03-06 20:55:06 +0800

    Create buying.js

---
+++ buying.js
@@ -0,0 +1,12 @@
+function confirmIt() {
+  return confirm('Are you Sure You Want To Buy This item?');
+}
+
+function showAlert() {
+    return confirm("You Brought An Item!");
+}
+function showanalert() {
+    alert("You Haven't Brought Anything Yet!");
+}
+
+
